7302476ed3a9219097738fdbc794e844cd41861f
Ana-Maria Cretescu <anax1030@edu.easj.dk>
2017-11-13 11:25:48 +0100

    implemented relaycommand class

--- FranceVacancesCentaurosTeam/Commands/RelayCommand.cs
+++ FranceVacancesCentaurosTeam/Commands/RelayCommand.cs
@@ -3,10 +3,43 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace FranceVacancesCentaurosTeam.Assets.Commands
 {
-    class RelayCommand
-    {
+    class RelayCommand : ICommand
+        {
+        
+        public event EventHandler CanExecuteChanged;
+
+              
+        private readonly Action<object> _targetExecuteMethod;
+
+               
+        private readonly Predicate<object> _targetCanExecuteMethod;
+
+        public bool CanExecute(object parameter)
+        {
+            return _targetCanExecuteMethod == null || _targetCanExecuteMethod(parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+        
+            if (_targetExecuteMethod != null) _targetExecuteMethod(parameter);
+        }
+
+        public RelayCommand(Action<object> executeMethod, Predicate<object> canExecuteMethod = null)
+        {
+            _targetExecuteMethod = executeMethod;
+            _targetCanExecuteMethod = canExecuteMethod;
+        }
+
+        public void RaiseCanExecuteChanged()
+        {
+            if (CanExecuteChanged != null) CanExecuteChanged(this, EventArgs.Empty);
+        }
     }
 }
+
+       
